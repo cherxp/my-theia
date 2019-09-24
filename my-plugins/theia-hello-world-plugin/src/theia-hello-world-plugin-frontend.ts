3960a29c848e6157278b0e0c7fc93e0c8f5ffebb
Rajesh Seenivasan <rajesh.seenivasan@gmail.com>
2019-09-24 18:59:50 +0530

    2nd commit

---
+++ my-plugins/theia-hello-world-plugin/src/theia-hello-world-plugin-frontend.ts
@@ -0,0 +1,22 @@
+
+/**
+ * Generated using theia-plugin-generator
+ */
+
+import * as theia from '@theia/plugin';
+
+export function start(context: theia.PluginContext) {
+    const informationMessageTestCommand = {
+        id: 'hello-world-example-generated',
+        label: "Hello World"
+    };
+    context.subscriptions.push(theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
+        //theia.window.showInformationMessage('Hello World!');
+        theia.mccServer.sayHello();
+    }));
+
+}
+
+export function stop() {
+
+}
